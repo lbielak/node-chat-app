c218795ee5702b7e120409e75da1630420aeffc2
BuiDuyThanh <buiduythanh6291taikhoan@gmail.com>
2017-10-03 07:41:56 +0300

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -20,11 +20,26 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
 	var formattedTime = moment(message.createdAt).format('h:mm a');
+	var template = jQuery('#message-template').html();
+	var html = Mustache.render(template, {
+		text: message.text,
+		from: message.from,
+		createdAt: formattedTime
+	});
+
+	jQuery('#messages').append(html);
+
+/*	---Without Mustache
+
+	var formattedTime = moment(message.createdAt).format('h:mm a');
 //	console.log('newMessage', message);
 	var li = jQuery('<li></li>');
 	li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
 	jQuery('#messages').append(li);
+
+*/
+
 });
 
 /*
@@ -38,6 +53,17 @@ socket.emit('createMessage', {
 
 socket.on('newLocationMessage', function (message) {
 	var formattedTime = moment(message.createdAt).format('h:mm a');
+	var template = jQuery('#location-message-template').html();
+	var html = Mustache.render(template, {
+		from: message.from,
+		url: message.url,
+		createdAt: formattedTime
+	});
+
+	jQuery('#messages').append(html);
+
+/*	---Without Mustache---
+	var formattedTime = moment(message.createdAt).format('h:mm a');
 
 	var li = jQuery('<li></li>');
 	var a = jQuery('<a target="_blank">My current location</a>');
@@ -46,6 +72,8 @@ socket.on('newLocationMessage', function (message) {
 	a.attr('href', message.url);
 	li.append(a);
 	jQuery('#messages').append(li);
+*/
+
 });
 
 jQuery('#message-form').on('submit', function(e) {
@@ -68,9 +96,11 @@ locationButton.on('click', function () {
 		return alert('Geolocation not supported by your browser.');
 	}
 
+	// disable Send Location button after sending, during the process
 	locationButton.attr('disabled', 'disabled').text('Sending location...');
 
 	navigator.geolocation.getCurrentPosition(function (position) {
+		// remove the disabled after sending location completed
 		locationButton.removeAttr('disabled').text('Send location');
 		socket.emit('createLocationMessage', {
 			latitude: position.coords.latitude,
